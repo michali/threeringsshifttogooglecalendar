e814f605ce93277ca7b723188e6aa5cad53d5811
Michali <michali@gmail.com>
2014-10-26 10:09:29 +0000

    Added tooltip to add to Google Calendar

--- addToCalendar.js
+++ addToCalendar.js
@@ -2,6 +2,7 @@ function addToCalendarOption(){
 	var myShifts = document.getElementsByClassName('rota_shift_slot rota_shift_filled_self saved');
 	for (var i = 0, len = myShifts.length; i < len; i++) {
 		var addToCalendar = document.createElement('a');
+		addToCalendar.setAttribute('title', 'Add to Google Calendar');
 		var image = document.createElement('img');
 		image.setAttribute('src',chrome.extension.getURL('googlecalendar.png'));
 		addToCalendar.appendChild(image);
