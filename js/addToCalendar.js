930015b334f6cb1496cbd3fc91f5ee580a8785a5
Michali <michali@gmail.com>
2014-11-08 02:26:59 +0000

    Extracted method that calculates shift calendar dates (as opposed to rota day dates)

--- js/addToCalendar.js
+++ js/addToCalendar.js
@@ -3,9 +3,7 @@ function addCalendarImage(addToCalendarLink) {
 		addToCalendarLink.appendChild(image);
 }
 
-function createAddToGoogleCalendarLink(shift, storage){
-	var addToCalendarLink = document.createElement('a');
-	addToCalendarLink.setAttribute('title', 'Add to Google Calendar');
+function getShiftCalendarDates(shift) {
 	var startDate = new Date(shift.startYear, shift.startMonth - 1, shift.startDay, shift.startHour, 0, 0, 0);
 	var endDate = new Date(shift.startYear, shift.startMonth - 1, shift.startDay, shift.endHour, 0, 0, 0);
 
@@ -16,13 +14,19 @@ function createAddToGoogleCalendarLink(shift, storage){
 	if (shiftTakesPlaceOnCalendarDayAfterCurrentRotaDay(shift)) {
 		startDate = startDate.addDays(1);
 		endDate = endDate.addDays(1);
-	}	
-	
-	storage.get({eventSubject: ''}, function(items) {
-		var event = new GoogleCalendarEvent(startDate, endDate, items.eventSubject);
+	}
+	return {startDate: startDate, endDate: endDate};
+}
+
+function createAddToGoogleCalendarLink(shift, storage) {
+	var addToCalendarLink = document.createElement('a');
+	addToCalendarLink.setAttribute('title', 'Add to Google Calendar');
+	var shiftCalendarDates = getShiftCalendarDates(shift);
+	storage.get({eventSubject: ''}, function (items) {
+		var event = new GoogleCalendarEvent(shiftCalendarDates.startDate, shiftCalendarDates.endDate, items.eventSubject);
 		addToCalendarLink.setAttribute('href', event.toLink());
 	});
- 		
+
 	return addToCalendarLink;
 }
 
