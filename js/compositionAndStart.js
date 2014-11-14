9f687788640140dbbfe4664be69efed5f988d3de
Michali <michali@gmail.com>
2014-11-14 00:28:35 +0000

    Removed view constructor, added view for fortnight and month page views

--- js/compositionAndStart.js
+++ js/compositionAndStart.js
@@ -2,7 +2,8 @@ function addToCalendarOption(){
 	var myShifts = documentAccessor.getElementsByClassName("rota_shift_slot rota_shift_filled_self saved");
 	for (var i = 0, len = myShifts.length; i < len; i++) {
 		if (myShifts[i].getElementsByTagName('img').length > 0) continue;
-		var shiftView = viewFinder.findView(myShifts[i]);
+		var shiftView = viewFinder.findView(documentAccessor.getUrl());
+		shiftView.populateShiftDateTimeFromDocument(myShifts[i]);
 		var addToCalendarLink = createAddToGoogleCalendarLink(shiftView, storage);
 		addCalendarImage(addToCalendarLink);
 		myShifts[i].appendChild(addToCalendarLink);
@@ -10,8 +11,7 @@ function addToCalendarOption(){
 }
 
 var documentAccessor = new DocumentAccessor();
-var url = documentAccessor.getUrl();
-var viewFinder = new ViewFinder(url);
+var viewFinder = new ViewFinder();
 var storage = new Storage();
 
 document.addEventListener('DOMNodeInserted', addToCalendarOption);
--- js/shiftViews.js
+++ js/shiftViews.js
@@ -1,36 +1,44 @@
-function WeekShiftView(shiftNode) {
-	var numberPattern = /\d+/g;
-	var containerWithShiftDateTimeInfoId = shiftNode.parentNode.parentNode.parentNode.parentNode.id;
-	var dateTimeElements = shiftTimeInfoId.match(numberPattern);
+function WeekShiftView() {
+	this.populateShiftDateTimeFromDocument = function(shiftNode) {
+		var containerWithShiftDateTimeInfoId = shiftNode.parentNode.parentNode.parentNode.parentNode.id;
+		var dateTimeElements = containerWithShiftDateTimeInfoId.match(numberPattern);
 
-	this.startHour = dateTimeElements[0];
-	this.endHour = dateTimeElements[2];
-	this.year = dateTimeElements[4];
-	this.month = dateTimeElements[5];
-	this.day = dateTimeElements[6];
+		this.startHour = dateTimeElements[0];
+		this.endHour = dateTimeElements[2];
+		this.year = dateTimeElements[4];
+		this.month = dateTimeElements[5];
+		this.day = dateTimeElements[6];
+	}
 }
 
-function FortnightShiftView(shiftNode) {
+var numberPattern = /\d+/g;
 
+function NonWeekShiftView() {
+	this.populateShiftDateTimeFromDocument = function(shiftNode) {
+		var containerWithShiftInfo = shiftNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+		var containerWithShiftTime = containerWithShiftInfo.getElementsByClassName("rota_item_time")[0];
+		var timeElements = containerWithShiftTime.innerText.match(numberPattern);
+		this.startHour = timeElements[0];
+		this.endHour = timeElements[2];
+
+		var rotaDayContainer = containerWithShiftTime.parentNode.parentNode;
+		var rotaDayContainerId = rotaDayContainer.id;
+		var dateElements = rotaDayContainerId.match(numberPattern);
+		this.year = dateElements[0];
+		this.month = dateElements[1];
+		this.day = dateElements[2];
+	}
 }
 
-function MonthShiftView(shiftNode) {
-
-}
-
-function ViewFinder(url) {
-	this.findView = function(shiftNode){
+function ViewFinder() {
+	this.findView = function(url){
 		var viewPattern = /style=\w+/;
 		var viewParams = url.match(viewPattern);
 
-		if (viewParams[0].endsWith("fortnight")){
-			return new FortnightShiftView(shiftNode);
+		if (viewParams[0].endsWith("week")){
+			return new WeekShiftView();
 		}
 
-		if (viewParams[0].endsWith("month")){
-			return new MonthShiftView(shiftNode);
-		}
-
-		return new WeekShiftView(shiftNode);
+		return new NonWeekShiftView();
 	}
 }
